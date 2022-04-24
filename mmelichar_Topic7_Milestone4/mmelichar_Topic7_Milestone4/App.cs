550aae9667b1c1101c56636c57e081dd2d4532ee
Michael Melichar <35645069+bynarywow@users.noreply.github.com>
2022-04-23 19:23:57 -0700

    cleaned up code

--- mmelichar_Topic7_Milestone4/mmelichar_Topic7_Milestone4/App.cs
+++ mmelichar_Topic7_Milestone4/mmelichar_Topic7_Milestone4/App.cs
@@ -8,10 +8,6 @@ namespace mmelichar_Topic7_Milestone4
 {
     public class App
     {
-        //array declaration no longer in use, don't need size/cap variables either
-        //Inventory[] inventory = new Inventory[10];
-        //int capacity = 10;
-        //int size = 0;
         Inventory inventory = new Inventory();
         List<Inventory> items = new List<Inventory>();
 
@@ -23,89 +19,31 @@ namespace mmelichar_Topic7_Milestone4
         {
             return inventory;
         }
-        /*
-         * helper function for exclusively arrays
-        public int getSize()
-        {
-            return size;
-        }
-        */
+
         public App()
         {
-            /*
-             * dummy data for my array
-            inventory[0] = new Inventory(00001, 25, "Lego Darth Vader Bust", 59.99);
-            inventory[1] = new Inventory(00002, 10, "Cool Ranch Doritos", 6.99);
-            inventory[2] = new Inventory(00003, 15, "Campbells Chicken Noodle Soup", 3.99);
-            size = 3;
-            */
+
         }
         public void addItem(Inventory item)
         {
+            //if null, create list
             if (items == null)
             {
                 items = new List<Inventory>();
             }
             items.Add(item);
-            
-            
-            /*
-             * array, no longer in use
-            if (inventory.Length >= capacity)
-            {
-                capacity *= 2;
-                Inventory[] temp = new Inventory[capacity];
-                int i = 0;
-                foreach (var inv in inventory)
-                {
-                    temp[i] = inventory[i];
-                    i++;
-                }
-
-                inventory = temp;
-            }
-
-            inventory[size] = item;
-            ++size;
-            return;
-            */
         }
         public void removeItem (int idx)
         {
+            //remove by index
             items.RemoveAt(idx); 
         }
         public void removeItem(Inventory item)
         {
-
-            items.Remove(item);
-                            //            items.Remove(item);
-            /*
-            int index = Array.IndexOf(inventory, item);
-            if (index != -1)
-            {
-                if (index != inventory.Length - 1)
-                {
-                    for (int i = index; i < size; i++)
-                    {
-                        inventory[i] = inventory[i + 1];
-                    }
-                }
-                else if (index == inventory.Length - 1)
-                {
-                    inventory[index] = null;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Result not found.");
-            }
-            */                                          
+            //remove by item object
+            items.Remove(item);                                          
         }
-            /*
-            List<Inventory> tmp = new List<Inventory>(inventory);
-            tmp.RemoveAt(index);
-            inventory = tmp.ToArray();
-            */
+            
 
         public void restockItem(int sku, int _quantity)
         {
@@ -130,27 +68,17 @@ namespace mmelichar_Topic7_Milestone4
         }
         public Inventory search(int sku, string description)
         {
-            //what needs to happen: Pass in an array of inventory items to search.
-            //Find all instances of the sku and the description individually, then
-            //compare the two lists of indexes to see if they are the same indexes.
-            //Only return the ones that are the same indexes.
-
+            //scan every item
             foreach (var item in items)
             {
+                //if sku + description match, return item
                 if (item.getSku() == sku && item.getDescription() == description)
                 {
                     return item;
                 }
             }
+            //if item not contained in list, return null
             return null;
-            //  Inventory item = new Inventory();
-            //  int[] skuIndex = inventory.Select((b, i) => b == sku ? i : -1).Where(i => i != -1).ToArray();
-            //int[] descIndex = Array.IndexOf(this.inventoryArray, description);
-            //if (index == nextIndex)
-            //{
-            //item = inventoryArray.ElementAt(index);
-            //}
-            //return item;
         }
     }
 }
--- mmelichar_Topic7_Milestone4/mmelichar_Topic7_Milestone4/Form1.cs
+++ mmelichar_Topic7_Milestone4/mmelichar_Topic7_Milestone4/Form1.cs
@@ -11,27 +11,7 @@ namespace mmelichar_Topic7_Milestone4
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //debug show prefab items on load
-            /*
-            System.Data.DataTable dt = new System.Data.DataTable("inv");
-            dt.Columns.Add("Sku", typeof(int));
-            dt.Columns.Add("Quantity", typeof(int));
-            dt.Columns.Add("Description", typeof(string));
-            dt.Columns.Add("Price", typeof(float));
 
-            Inventory[] list = app.getInventory();
-            for(int i=0;i<app.getSize();i++)
-            {
-                DataRow dr = dt.NewRow();
-                dr["Sku"] = list[i].getSku();
-                dr["Quantity"] = list[i].getQuantity();
-                dr["Description"] = list[i].getDescription();
-                dr["Price"] = list[i].getPrice();
-                dt.Rows.Add(dr);
-
-            }
-            dataGridView1.DataSource = dt;
-            */
         }
 
        
@@ -47,6 +27,7 @@ namespace mmelichar_Topic7_Milestone4
         {
             int value;
             double filler;
+            //verify input before parse
             if (int.TryParse(textBoxSku.Text, out value) && int.TryParse(textBoxQuantity.Text, out value) && double.TryParse(textBoxPrice.Text, out filler))
             {
                 //parsing successful
@@ -58,11 +39,13 @@ namespace mmelichar_Topic7_Milestone4
                 //parsing failed. 
                 MessageBox.Show("Invalid input. Please check that SKU, Quantity, and Price only contain numbers.");
             }
+            //refresh display
             display();
         }
 
         private void btnDisplayInventory_Click(object sender, EventArgs e)
         {
+            //refresh display
             display();
         }
         public void display()
@@ -93,68 +76,37 @@ namespace mmelichar_Topic7_Milestone4
 
    
         private void btnRemoveItem_Click(object sender, EventArgs e) { 
-            int value;
-            double filler;
-
+            //grab index of selected item
             int idx = dataGridView1.CurrentRow.Index;
+            //remove it
             app.removeItem(idx);
+            //refresh display
             display();
-
-/*  
-
-            if (int.TryParse(textBoxSku.Text, out value) && int.TryParse(textBoxQuantity.Text, out value) && double.TryParse(textBoxPrice.Text, out filler))
-            {
-                //parsing successful
-                Inventory item = new Inventory(int.Parse(textBoxSku.Text), int.Parse(textBoxQuantity.Text), textBoxDescription.Text, double.Parse(textBoxPrice.Text));
-                MessageBox.Show(item.ToString());
-                app.removeItem(item);
-
-                //debug reload table on addition
-                System.Data.DataTable dt = new System.Data.DataTable("inv");
-                dt.Columns.Remove("Sku");
-                dt.Columns.Remove("Quantity");
-                dt.Columns.Remove("Description");
-                dt.Columns.Remove("Price");
-
-                List<Inventory> list = app.getList();
-                for (int i = 0; i < list.Count(); i++)
-                {
-                    DataRow dr = dt.NewRow();
-                    dr["Sku"] = list[i].getSku();
-                    dr["Quantity"] = list[i].getQuantity();
-                    dr["Description"] = list[i].getDescription();
-                    dr["Price"] = list[i].getPrice();
-                    dt.Rows.Add(dr);
-
-                }
-                dataGridView1.DataSource = dt;
-                   
-            }
-            else
-            {
-                //parsing failed. 
-                MessageBox.Show("Invalid input. Please check that SKU, Quantity, and Price only contain numbers.");
-            }
-*/
         }
 
         public void btnRestockItem_Click(object sender, EventArgs e)
         {
             Form2 restockDialog = new Form2();
             restockDialog.ShowDialog();
+            //get sku/count from form2
             int sku = restockDialog.getSku();
             int count = restockDialog.getCount();
+            //use sku/count to restock qty
             app.restockItem(sku, count);
+            //refresh display
             display();
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            //search for item
             Inventory item = app.search(Int32.Parse(textBoxSku.Text), textBoxDescription.Text);
+            //if not found
             if(item==null)
             {
                 MessageBox.Show("Not Found");
             }
+            //otherwise print item
             else
             {
                 MessageBox.Show(item.ToString());
